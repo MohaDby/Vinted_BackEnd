7878a178337859b99b9d043befefeee2555f4383
Mohamed <MohaDby@icloud.com>
2024-03-10 16:09:19 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2; // ne pas oublier le v2
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const signupRoutes = require("./routes/signup");
 app.use(signupRoutes);
@@ -14,18 +17,18 @@ app.use(loginRoutes);
 const offerRoutes = require("./routes/offer");
 app.use(offerRoutes);
 
-mongoose.connect("mongodb://localhost:27017/vinted");
+mongoose.connect("process.env.MONGODB_URI");
 
 cloudinary.config({
-  cloud_name: "dsdxpztux",
-  api_key: "811538252972272",
-  api_secret: "DpLsjvLmHRzDjJaMqikrm1WW6m8",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 }); // la config pour pouvoir se connecter
 
 app.all("*", (req, res) => {
   res.status(400).json({ message: "cette page n'existe pas" });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server started");
 });
